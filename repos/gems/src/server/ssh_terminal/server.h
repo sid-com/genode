4ed3cb71c3314f56a2a2f9ec28d56fc73029f51e
Josef Söntgen <josef.soentgen@genode-labs.com>
2019-06-12 16:42:16 +0200

    fixup 'ssh: add exec channel and exit on interactive' (review)
    Issue #3401.

--- repos/gems/src/server/ssh_terminal/server.h
+++ repos/gems/src/server/ssh_terminal/server.h
@@ -102,8 +102,8 @@ struct Ssh::Terminal_session : Genode::Registry<Terminal_session>::Element
 
 	Terminal_session(Genode::Registry<Terminal_session> &reg,
 	                 Ssh::Terminal &conn,
-	                 ssh_event event_loop) :
-		Element{ reg, *this }, conn{ conn }, _event_loop{ event_loop }
+	                 ssh_event event_loop)
+	: Element(reg, *this), conn(conn), _event_loop(event_loop)
 	{
 		if (pipe(_fds) ||
 			ssh_event_add_fd(_event_loop,
@@ -198,7 +198,6 @@ class Ssh::Server
 		 */
 
 		static void *_server_loop(void *arg);
-		static int _stderr_avail_cb(socket_t fd, int revents, void *userdata);
 
 		bool _allow_multi_login(ssh_session s, Login const &login);
 
@@ -277,17 +276,5 @@ class Ssh::Server
 		                 struct ssh_key_struct *pubkey,
                          char signature_state);
 };
-
-
-static int write_avail_cb(socket_t fd, int revents, void *userdata)
-{
-	int n = 0;
-	Libc::with_libc([&] {
-		char c;
-		n = ::read(fd, &c, sizeof(char));
-	});
-	return n;
-}
-
 
 #endif /* _SSH_TERMINAL_SERVER_H_ */
