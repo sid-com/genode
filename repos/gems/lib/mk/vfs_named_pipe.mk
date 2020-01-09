cae4f94b460b6e3b66cab1b703413f17547fb885
Sid Hussmann <sid.hussmann@gapfruit.com>
2020-01-09 16:58:19 +0100

    vfs: remove named pipe plugin
    The named pipe feature is implemented in the vfs pipe plugin.
    
    Issue #3583

--- repos/gems/lib/mk/vfs_named_pipe.mk
+++
@@ -1,5 +0,0 @@
-SRC_CC = file_system.cc pipe_handle.cc plugin.cc pipe.cc
-
-vpath %.cc $(REP_DIR)/src/lib/vfs/named_pipe
-
-SHARED_LIB = yes
--- repos/gems/src/lib/vfs/named_pipe/target.mk
+++
@@ -1,2 +0,0 @@
-TARGET = dummy-vfs_named_pipe
-LIBS   = vfs_named_pipe
